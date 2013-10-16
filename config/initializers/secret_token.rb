2fb51b3ccb6d62374ad410963ab308acbf48f84e
John Messenger <JMessenger@iee.org>
2013-10-16 22:31:37 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hartlapp::Application.config.secret_key_base = 'a24eebe67d5455d34c5b25eef693ace362ef311debb7e139ac976c895efa26bafc7723497529ea5ec70213e07d0de271e1f3e8dc7ce0eb6a90ea71dbfaf511be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hartlapp::Application.config.session_store :cookie_store, key: '_hartlapp_session'
