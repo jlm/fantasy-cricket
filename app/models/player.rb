92c1403945d9da79c097a2952d6ab1a59ae7267b
John Messenger <JMessenger@iee.org>
2013-11-02 17:27:53 +0000

    Make averages floating point numbers. Add maidens to bowling scores

--- app/models/player.rb
+++ app/models/player.rb
@@ -12,13 +12,13 @@ class Player < ActiveRecord::Base
   private
   	def update_player_scores
   	  self.bat_score = bat_runs_scored + bat_fifties*25 + bat_hundreds*50 + bat_not_outs*5 - bat_ducks*8
-  	  self.bowl_score = bowl_wickets*15 + bowl_4_wickets*25 + bowl_6_wickets*50
+  	  self.bowl_score = bowl_wickets*15 + bowl_4_wickets*25 + bowl_6_wickets*50 + bowl_maidens*4
   	  self.field_score = (field_catches + field_runouts + field_stumpings)*15 - field_drops*5
   	  self.bonus = field_mom*20
   	  self.total = self.bat_score + self.bowl_score + self.field_score + self.bonus
   	  self.bat_avg_invalid = (bat_innings - bat_not_outs == 0)
-	  self.bat_avg = bat_runs_scored / (bat_innings - bat_not_outs) unless self.bat_avg_invalid
+	  self.bat_avg = (bat_runs_scored + 0.0) / (bat_innings - bat_not_outs) unless self.bat_avg_invalid
   	  self.bowl_avg_invalid = (bowl_wickets == 0)
-      self.bowl_avg = bowl_runs / bowl_wickets unless self.bowl_avg_invalid
+      self.bowl_avg = (bowl_runs + 0.0) / bowl_wickets unless self.bowl_avg_invalid
   	end
 end
