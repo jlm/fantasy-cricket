2d2d86480f1b3f44f3ae26da5247398537602b71
John Messenger <JMessenger@iee.org>
2013-12-01 22:22:11 +0000

    Changes to make SimpleForm work better with Bootstrap 3, and some visual adjustments.

---
+++ config/initializers/remove-me-sf_bs3_inputs.rb
@@ -0,0 +1,25 @@
+# This came from http://www.iconoclastlabs.com/blog/using-twitter-bootstrap-3-with-simple_form
+
+inputs = %w[
+  CollectionSelectInput
+  DateTimeInput
+  FileInput
+  GroupedCollectionSelectInput
+  NumericInput
+  PasswordInput
+  RangeInput
+  StringInput
+  TextInput
+]
+
+inputs.each do |input_type|
+  superclass = "SimpleForm::Inputs::#{input_type}".constantize
+
+  new_class = Class.new(superclass) do
+    def input_html_classes
+      super.push('form-control')
+    end
+  end
+
+  Object.const_set(input_type, new_class)
+end
