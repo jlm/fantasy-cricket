ccaac5b75893ac4b983310a708763c91560366d3
John Messenger <JMessenger@iee.org>
2013-12-05 23:14:48 +0000

    Set up the database seeding with the default users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: "The Admin Guy",
+ email: "cricketadmin@cuthberts.org.uk",
+ password: "foobar",
+ password_confirmation: "foobar",
+ admin: true,
+ totalscore: 0,
+ teamcash: 6000,
+ drop_available: true)
+User.create!(name: "Lemon Grass",
+ email: "lemongrass@lemongrass.org.uk",
+ password: "foobar",
+ password_confirmation: "foobar",
+ admin: false,
+ totalscore: 0,
+ teamcash: 6000,
+ drop_available: true)
+User.create!(name: "Tom Messenger",
+ email: "tom@cuthberts.org.uk",
+ password: "ThomasM1",
+ password_confirmation: "ThomasM1",
+ admin: true,
+ totalscore: 0,
+ teamcash: 6000,
+ drop_available: true)
+   
