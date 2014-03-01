7bad60d6089f1517eeb755ee39251336b4b41377
John Messenger <JMessenger@iee.org>
2014-03-01 22:03:52 +0000

    Fix typos in the mailer module.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
   def invite_email(token)
   	@token = token
   	#binding.pry
-  	@url = 'http://cricket.cuthberts.org/signup?email=' + CGI.escape(@token.email) +
+  	@url = 'http://cricket.cuthberts.org.uk/signup?email=' + CGI.escape(@token.email) +
   		'&name=' + CGI.escape(@token.realname) +
   		'&tokenstr=' + CGI.escape(@token.tokenstr)
   	mail(to: @token.email, subject: 'Sign up for Helperby Fantasy Cricket')
