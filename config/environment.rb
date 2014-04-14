d4b356d87b50b3eef2f9cc70d44d2ecd6efd72af
John Messenger <JMessenger@iee.org>
2014-04-15 00:38:47 +0100

    Incraese initial teamcash to £8.5m per player (£93.5m)

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ AGE_CATEGORIES = %w(Junior Adult)
 PLAYER_CATEGORIES = %w(batsman bowler all-rounder keeper)
 INITIAL_PLAYER_PRICES = [0, 1000, 800, 600]
 # Budget is average player price for each team player.
-INITIAL_TEAMCASH = PLAYERS_PER_TEAM * 69
+INITIAL_TEAMCASH = PLAYERS_PER_TEAM * 85
 
 # Initialize the Rails application.
 Hartlapp::Application.initialize!
